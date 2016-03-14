23a343cb6a69e785f3e59264319645462fdd98fd
henryiz <web.dzrakir@gmail.com>
2016-03-14 17:08:08 +0300

    I add a new file

---
+++ lesson1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Example
+{
+	class Program
+	{
+		static void Main()
+		{
+			System.Console.WriteLine("Hello!"); // Вывод заданного текста в консоль
+			System.Console.Read(); // Ожидание нажатия клавиши пользователем
+		}
+
+	}
+}
